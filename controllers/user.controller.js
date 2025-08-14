196d3991af3b83d6885d5bb25b390ccf19a3ca1b
Your Name <your-email@example.com>
2025-08-14 12:46:15 +0530

    frontend

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,66 +1,6 @@
 
 
 
-// import User from "../models/user.model.js";
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-// const SECRET_KEY = process.env.JWT_SECRET; // <-- load from .env
-
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, SECRET_KEY, { expiresIn: "1d" });
-// };
-
-// export default {
-//   createUser: async (req, res) => {
-//     try {
-//       const user = await User.create(req.body);
-//       res.status(201).json(user);
-//     } catch (err) {
-//       res.status(500).json({ message: err.message });
-//     }
-//   },
-
-//   getUsers: async (req, res) => {
-//     try {
-//       const users = await User.find().populate("role");
-//       res.json(users);
-//     } catch (err) {
-//       res.status(500).json({ message: err.message });
-//     }
-//   },
-
-//   loginUser: async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     console.log(req.body);
-    
-//     const user = await User.findOne({ email }).populate("role");
-//     console.log("role",user);
-    
-
-//     if (!user || !(await user.matchPassword(password))) {
-//       return res.status(401).json({ message: "Invalid credentials" });
-//     }
-
-//     res.json({
-//       message: "Login successful",
-//       _id: user._id,
-//       name: `${user.firstName} ${user.lastName}`,
-//       email: user.email,
-//       role: user.role.name,
-//       token: generateToken(user._id),
-//     });
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// }
-// ,
-// };
-
-
-
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
