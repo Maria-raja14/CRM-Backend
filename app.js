a48eaf4643837c9d979aa936d539242bb87b49c3
Your Name <your-email@example.com>
2025-08-14 12:34:13 +0530

    frontend

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import routes from './routes/index.routes.js'
 
 import cors from 'cors'
 
-import userRoutes from "./routes/user.route.js";
-import roleRoutes from "./routes/role.routes.js";
+// import userRoutes from "./routes/user.route.js";
+// import roleRoutes from "./routes/role.routes.js";
 
 
 dotenv.config();
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //middleware
 app.use("/api",routes)
-app.use("/api/users", userRoutes);
-app.use("/api/roles", roleRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/roles", roleRoutes);
 // app.use("/api", profileRoutes);  
 // app.use("/api", passwordRoutes);
 // app.use("/api", socialLinksRoutes);
@@ -34,15 +34,12 @@ app.use("/uploads", express.static("uploads"));
 const PORT=process.env.PORT || 5000;
 
 
-// app.listen(PORT,async()=>{
-//     console.log(`Server Running on ${PORT}`)
-//     await connectDB();
-// })
+app.listen(PORT,async()=>{
+    console.log(`Server Running on ${PORT}`)
+    await connectDB();
+})
 
 
 
-app.listen(PORT, '0.0.0.0', async () => {
-  console.log(`Server Running on is ${PORT}`);
-  await connectDB();
-});
+
 
