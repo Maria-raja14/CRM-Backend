35819813dfd5b592bfe26a628904abeb5a09f7c6
Yasar-1225 <yasarj.techzar@gmail.com>
2025-08-20 16:16:40 +0530

    Frontend

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -88,48 +88,100 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const generateToken = (id) => jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: "1d" });
 
 export default {
-  createUser: async (req, res) => {
-    try {
-      const {
-        firstName,
-        lastName,
-        email,
-        password,
-        mobileNumber,
-        role,
-        status,
-        gender,
-        address,
-        dateOfBirth
-      } = req.body;
+  // createUser: async (req, res) => {
+  //   try {
+  //     const {
+  //       firstName,
+  //       lastName,
+  //       email,
+  //       password,
+  //       mobileNumber,
+  //       role,
+  //       status,
+  //       gender,
+  //       address,
+  //       dateOfBirth
+  //     } = req.body;
 
-      const profileImage = req.file ? req.file.path : null;
+  //     const profileImage = req.file ? req.file.path : null;
 
-      const user = await User.create({
-        firstName,
-        lastName,
-        email,
-        password,
-        mobileNumber,
-        role,
-        status,
-        gender,
-        address,
-        dateOfBirth,
-        profileImage
-      });
+  //     const user = await User.create({
+  //       firstName,
+  //       lastName,
+  //       email,
+  //       password,
+  //       mobileNumber,
+  //       role,
+  //       status,
+  //       gender,
+  //       address,
+  //       dateOfBirth,
+  //       profileImage
+  //     });
 
-      res.status(201).json(user);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  //     res.status(201).json(user);
+  //   } catch (err) {
+  //     res.status(500).json({ message: err.message });
+  //   }
+  // },
+
+createUser : async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      mobileNumber,
+      role,
+      status,
+      gender,
+      address,
+      dateOfBirth
+    } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      // Remove uploaded file if user already exists
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+      return res.status(400).json({ message: "User already exists with this email" });
     }
-  },
+
+    const profileImage = req.file ? req.file.path : null;
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      mobileNumber,
+      role,
+      status,
+      gender,
+      address,
+      dateOfBirth,
+      profileImage
+    });
+
+    res.status(201).json(user);
+  } catch (err) {
+    // Remove uploaded file if error occurs
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
+    res.status(500).json({ message: err.message });
+  }
+},
 
   getUsers: async (req, res) => {
     try {
@@ -154,26 +206,99 @@ export default {
     }
   },
 
-  updateUser: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const updateData = req.body;
+  // updateUser: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+  //     const updateData = req.body;
 
-      if (req.file) {
-        updateData.profileImage = req.file.path;
-      }
+  //     if (req.file) {
+  //       updateData.profileImage = req.file.path;
+  //     }
 
-      const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true }).populate("role");
+  //     const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true }).populate("role");
       
-      if (!updatedUser) {
-        return res.status(404).json({ message: "User not found" });
-      }
+  //     if (!updatedUser) {
+  //       return res.status(404).json({ message: "User not found" });
+  //     }
 
-      res.json(updatedUser);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  //     res.json(updatedUser);
+  //   } catch (err) {
+  //     res.status(500).json({ message: err.message });
+  //   }
+  // },
+
+ updateUser : async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      firstName,
+      lastName,
+      email,
+      mobileNumber,
+      role,
+      status,
+      gender,
+      address,
+      dateOfBirth
+    } = req.body;
+
+    // Find user
+    const user = await User.findById(id);
+    if (!user) {
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+      return res.status(404).json({ message: "User not found" });
     }
-  },
+
+    // Check if email is being changed and if it's already taken
+    if (email && email !== user.email) {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        if (req.file) {
+          fs.unlinkSync(req.file.path);
+        }
+        return res.status(400).json({ message: "Email already taken" });
+      }
+    }
+
+    // If new image is uploaded, delete the old one
+    let profileImage = user.profileImage;
+    if (req.file) {
+      // Delete old image if it exists
+      if (user.profileImage && fs.existsSync(user.profileImage)) {
+        fs.unlinkSync(user.profileImage);
+      }
+      profileImage = req.file.path;
+    }
+
+    // Update user
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        email,
+        mobileNumber,
+        role,
+        status,
+        gender,
+        address,
+        dateOfBirth,
+        profileImage
+      },
+      { new: true, runValidators: true }
+    ).populate("role");
+
+    res.json(updatedUser);
+  } catch (err) {
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
+    res.status(500).json({ message: err.message });
+  }
+},
+
 
   deleteUser: async (req, res) => {
     try {
