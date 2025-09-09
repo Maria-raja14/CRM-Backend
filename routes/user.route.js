cdf14c86333623876178f68ca71c4e650c6ec8d7
Yasar-1225 <yasarj.techzar@gmail.com>
2025-09-09 18:26:14 +0530

    Frontend

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,21 +1,6 @@
-
-// import express from "express";
-// import indexControllers from "../controllers/index.controllers.js";
-// import { protect, adminOnly } from "../middlewares/auth.middleware.js";
-// import upload from "../middlewares/upload.js";
-
-// const router = express.Router();
-
-// router.post("/create",upload.single("profileImage"), protect, adminOnly, indexControllers.usersController.createUser);
-// router.get("/", protect, adminOnly, indexControllers.usersController.getUsers);
-// router.post("/login", indexControllers.usersController.loginUser);
-
-// export default router;
-
-
 import express from "express";
 import indexControllers from "../controllers/index.controllers.js";
-import { protect, adminOnly } from "../middlewares/auth.middleware.js";
+import { protect, adminOnly,adminOrAssigned, } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.js";
 
 const router = express.Router();
@@ -26,5 +11,9 @@ router.put("/update-user/:id", upload.single("profileImage"), protect, adminOnly
 router.delete("/delete-user/:id", protect, adminOnly, indexControllers.usersController.deleteUser);
 router.post("/login", indexControllers.usersController.loginUser);
 router.put("/update-password", protect, indexControllers.usersController.updatePassword);
+
+router.post("/forgot-password", indexControllers.usersController.forgotPassword);
+router.post("/reset-password/:token", indexControllers.usersController.resetPassword);
+
 
 export default router;
